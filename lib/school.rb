a34037198757636ba004a78a98b4ae93aa765cdd
Susan Meyers <smmeyers24@gmail.com>
2018-09-04 23:20:36 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -30,8 +30,8 @@ end
 
 def sort
   roster.map do |grades, students|
-  roster.sort_by {|key_value, key|}
-   roster[key_value] => key
+  roster.sort_by
+   roster.sort_by[key_value] => key
   end
 end
 
