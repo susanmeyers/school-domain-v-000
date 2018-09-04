3f9b0d1903025347bdab720975818f318ea23e86
Susan Meyers <smmeyers24@gmail.com>
2018-09-04 22:19:52 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
-# code here!
+class School
+
+  attr_accessor :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+
+def add_student(name, grade)
+
+     roster[grade] || roster[grade] = []
+     roster[grade] << name
+ end
+
+ #   if roster[grade] == nil
+ #   roster[grade] = []
+ #   roster[grade] << name
+ #
+ #   else
+ #     roster[grade] << name
+ #   end
+ # end
+
+def grade(grade)
+  roster(grade).each do |student, grades|
+    if roster.include? {grades}
+      return student
+
+  end
+end
+
+
+
+
+
+
+
+
+
+end
